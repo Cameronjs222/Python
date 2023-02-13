7108c6d68e374b1776647e3ff07e00bb56ddd569
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-13 13:50:09 -0800

    added new user

--- oop/user.py
+++ oop/user.py
@@ -6,3 +6,18 @@ class User:
         self.age = age
     is_rewards_member = False
     gold_card_points = 0
+    def display_info(self):
+        print(self.first_name)
+        print(self.last_name)
+        print(self.email)
+        print(self.age)
+    def enroll(self):
+        self.is_rewards_member = True
+        self.gold_card_points += 200
+    def spend_points(self,amount):
+        self.gold_card_points -= amount
+cameron = User("Cameron", "Shaffer", "cameronjs@email.com", 27)
+alex = User("Alex", "Schaffer", "Alex@email.com", 25)
+cameron.display_info()
+cameron.enroll()
+cameron.spend_points(50)
