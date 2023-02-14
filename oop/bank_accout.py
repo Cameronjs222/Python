9dae2c55e1781105b1442ba4e4d108906f60cbd7
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-14 13:22:08 -0800

    user linked class

--- oop/bank_accout.py
+++ oop/bank_accout.py
@@ -24,3 +24,25 @@ user1=BankAccount(5, 500)
 user2=BankAccount(5, 10000)
 user1.deposit(100).deposit(500).deposit(50).withdraw(70).yield_interest().display_account_info()
 user2.deposit(1000).deposit(5000).withdraw(500).withdraw(100).withdraw(400).withdraw(1000).yield_interest().display_account_info()
+class User:
+    def __init__(self, first, last, email, age, int, balance) -> None:
+        self.first_name = first
+        self.last_name = last
+        self.email = email
+        self.age = age
+        self.account = BankAccount(int, balance)
+    def make_deposit(self, amount):
+        self.account.money += amount
+        self.account.balance = f"${str(self.account.money)}"
+        return self
+    def make_withdraw(self, amount):
+        self.account.money -= amount
+        self.account.balance = f"${str(self.account.money)}"
+        return self
+    def display_account_info(self):
+        display = f"Balance: {self.account.balance}"
+        print(display)
+        return display
+user1 = User('Cameron', 'Shaffer', 'Cameron@email.com', 27, 5, 500)
+user1.account.deposit(800).yield_interest().display_account_info()
+user1.account.withdraw(200).yield_interest().display_account_info()
