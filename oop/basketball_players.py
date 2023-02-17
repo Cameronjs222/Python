72bca9cf901c88d5f392b58370e3cb6effed5404
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-17 13:16:23 -0800

    needs work

---
+++ oop/basketball_players.py
@@ -0,0 +1,52 @@
+class Player:
+    def __init__(self, dict):
+        self.name = dict["name"]
+        self.age = dict["age"]
+        self.position = dict["position"]
+        self.team = dict["team"]
+
+
+players = [
+    {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving", 
+        "age":32, "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard", 
+        "age":33, "position": "Point Guard", 
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid", 
+        "age":32, "position": "Power Foward", 
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "", 
+        "age":16, 
+        "position": "P", 
+        "team": "en"
+    }
+]
+
+def player(dict, Class):
+    player_info = {}
+    for i in range(len(dict)):
+        player_info[dict[i]["name"]] = Class(dict)
+        # player_info[i] = Class(list[i]["name"], list[i]["age"], list[i]["position"], list[i]["team"])
+    return player_info
+player_info = player(players, Player)
+print(player_info.keys())
