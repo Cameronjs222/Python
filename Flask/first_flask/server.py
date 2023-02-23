988df88a4e525b472d23cabed1368d2f8e0813a9
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-23 15:04:26 -0800

    playground flask files

--- Flask/first_flask/server.py
+++ Flask/first_flask/server.py
@@ -18,3 +18,4 @@ def repeat(string, num):
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
 
+@app
