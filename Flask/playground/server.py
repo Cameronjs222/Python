e2263d9bea6de59fa90385eb43eea752da0a2056
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-24 12:26:21 -0800

    updating

--- Flask/playground/server.py
+++ Flask/playground/server.py
@@ -3,5 +3,8 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')
 def hello():
     return render_template("index.html")
+@app.route('/repeat/<int:num>/<string:name>')
+def repeat(name, num):
+    return render_template("hello.html", hello_num = num, hello_name = name)
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
