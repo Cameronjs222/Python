c7b3d6a4b61e6238c3ba6885a71037600e6f0675
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-24 17:44:53 -0800

    finished flask app that is adjusted based on input

--- Flask/playground/server.py
+++ Flask/playground/server.py
@@ -3,8 +3,15 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')
 def hello():
     return render_template("index.html")
-@app.route('/repeat/<int:num>/<string:name>')
-def repeat(name, num):
-    return render_template("hello.html", hello_num = num, hello_name = name)
+@app.route('/play')
+def squares():
+    return render_template("play.html", amount = 3, version = "teal")
+@app.route('/play/<int:num>')
+def playground_squares_num(num):
+        return render_template("play.html", amount=num, version = "teal")
+@app.route('/play/<int:num>/<string:color>')
+def playground_squares_num_color(num, color):
+    return render_template("play.html", amount=num, version=color)
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
