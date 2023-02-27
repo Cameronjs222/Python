b495f6f4d1261f651262b32db50ac5d01a3f00db
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-27 15:23:20 -0800

    added more routes

--- Flask/Checkerbox/server.py
+++ Flask/Checkerbox/server.py
@@ -4,12 +4,18 @@ def box(rows, boxs, color_1, color_2):
     return render_template("index.html", rows=rows, boxs=boxs,color_1=color_1, color_2=color_2, width=100*boxs)
 @app.route("/")
 def route1():
-    return box()
-@app.route("/hello")
-def route2():
-    return box()
+    return box(2, 2, "black", "rgb(237,237,237)")
+@app.route("/<int:num>")
+def route2(num):    
+    return box(int(num/2) + (num % 2 > 0), 2, "black", "rgb(237,237,237)")
+@app.route("/<int:num1>/<int:num2>")
+def route3(num1, num2):
+    return box(int(num1/2) + (num1 % 2 > 0), int(num2/2) + (num2 % 2 > 0), "black", "rgb(237,237,237)",)
+@app.route("/<int:num1>/<int:num2>/<string:color1>")
+def route4(num1, num2, color1):
+    return box(int(num1/2) + (num1 % 2 > 0), int(num2/2) + (num2 % 2 > 0), color1, "rgb(237,237,237)",)
 @app.route("/<int:num1>/<int:num2>/<string:color1>/<string:color2>")
 def route_end(num1, num2, color1, color2):
-    return box(int(num1/2) + (num1 % 2 > 0), int(num2/2) + (num1 % 2 > 0), color1, color2,)
+    return box(int(num1/2) + (num1 % 2 > 0), int(num2/2) + (num2 % 2 > 0), color1, color2,)
 if __name__ == "__main__":
     app.run(debug=True)
