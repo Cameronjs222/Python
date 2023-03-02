4ea202deef04ce04b93967cd89ca8ba770d708bd
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-03-02 02:51:26 -0800

    set everything up need to include sessions!

--- Flask/survey/server.py
+++ Flask/survey/server.py
@@ -3,5 +3,10 @@ app = Flask(__name__)
 @app.route("/")
 def survey():
     return render_template("index.html")
+@app.route('/process',methods=['POST'])
+def process():
+    print("Got Post Info")
+    print(request.form)
+    return "hello world"
 if __name__ == "__main__":
     app.run(debug=True)
