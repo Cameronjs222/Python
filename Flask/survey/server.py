a19d303ac99e62f2f373d1f96f93b6d4f3ca0071
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-03-02 17:12:19 -0800

    update ready to build second page

--- Flask/survey/server.py
+++ Flask/survey/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe' 
 @app.route("/")
 def survey():
     return render_template("index.html")
@@ -7,6 +8,13 @@ def survey():
 def process():
     print("Got Post Info")
     print(request.form)
-    return "hello world"
+    session['user'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comment'] = request.form['comment']
+    return redirect('/display')
+@app.route('/display')
+def display():
+    return session['user']
 if __name__ == "__main__":
     app.run(debug=True)
