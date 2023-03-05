505e1b47717d4ce5caf9c5dcc805074d2bcfaf08
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-03-05 14:30:47 -0800

    Setting up user server files

---
+++ Flask/user_cd/user.py
@@ -0,0 +1,24 @@
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+# model the class after the user table from our database
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.occupation = data['occupation']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+# Now we use class methods to query our database
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL('first_flask').query_db(query)
+        # Create an empty list to append our instances of users
+        users = []
+        # Iterate over the db results and create instances of users with cls.
+        for user in results:
+            users.append( cls(user) )
+        return users
+            
