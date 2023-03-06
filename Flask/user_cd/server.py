21fc828aa5e2940e3b06b8db935030c0c74a0322
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-03-05 23:27:56 -0800

    updated to users_schema for submission

--- Flask/user_cd/server.py
+++ Flask/user_cd/server.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
-# import the class from friend.py
+from flask import Flask, render_template, request, redirect
 from user import User
 app = Flask(__name__)
 @app.route("/")
 def index():
-    # call the get all classmethod to get all friends
+    return render_template("index.html")
+@app.route("/users")
+def users():
     users = User.get_all()
     print(users)
     return render_template("users.html", users=users)
-            
+@app.route('/create_user', methods=["POST"])
+def create_user():
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "mail" : request.form["mail"]
+    }
+    User.save(data)
+    return redirect('/users')
 if __name__ == "__main__":
     app.run(debug=True)
 
--- Flask/user_cd/user.py
+++ Flask/user_cd/user.py
@@ -1,8 +1,4 @@
-#TODO add new class method that adds users to the database as this progam only pulls in and creates a list from an existing database. Make secure?
-
-# import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
-# model the class after the user table from our database
 class User:
     def __init__( self , data ):
         self.id = data['id']
@@ -11,21 +7,15 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-# Now we use class methods to query our database
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        # make sure to call the connectToMySQL function with the schema you are targeting.
-        results = connectToMySQL('USER_CD').query_db(query)
-        # Create an empty list to append our instances of users
+        results = connectToMySQL('users_schema').query_db(query)
         users = []
-        # Iterate over the db results and create instances of users with cls.
         for user in results:
             users.append( cls(user) )
         return users
-    # @classmethod
-    # def insert_user(cls):
-    #     query = 'INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES (self.first_name, self.last_name, self.email, "NOW()","NOW() ON UPDATE NOW()")'
-    #     connectToMySQL('user_cd').query_db(query)
-
-# This won't work as this class seems to be built to pull only. I need to think of something else to insert into users.
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO users ( first_name , last_name , email) VALUES ( %(fname)s , %(lname)s , %(mail)s);"
+        return connectToMySQL('users_schema').query_db( query, data )
