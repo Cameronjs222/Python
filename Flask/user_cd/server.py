5ac9ac1b81c12632cc614485b20dfd9614ba2471
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-03-05 17:22:21 -0800

    updates

--- Flask/user_cd/server.py
+++ Flask/user_cd/server.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # call the get all classmethod to get all friends
-    
     users = User.get_all()
     print(users)
-    return render_template("index.html")
+    return render_template("users.html", users=users)
             
 if __name__ == "__main__":
     app.run(debug=True)
--- Flask/user_cd/user.py
+++ Flask/user_cd/user.py
@@ -1,3 +1,5 @@
+#TODO add new class method that adds users to the database as this progam only pulls in and creates a list from an existing database. Make secure?
+
 # import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
 # model the class after the user table from our database
@@ -6,7 +8,7 @@ class User:
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
-        self.occupation = data['occupation']
+        self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 # Now we use class methods to query our database
@@ -14,11 +16,16 @@ class User:
     def get_all(cls):
         query = "SELECT * FROM users;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
-        results = connectToMySQL('first_flask').query_db(query)
+        results = connectToMySQL('USER_CD').query_db(query)
         # Create an empty list to append our instances of users
         users = []
         # Iterate over the db results and create instances of users with cls.
         for user in results:
             users.append( cls(user) )
         return users
-            
+    # @classmethod
+    # def insert_user(cls):
+    #     query = 'INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES (self.first_name, self.last_name, self.email, "NOW()","NOW() ON UPDATE NOW()")'
+    #     connectToMySQL('user_cd').query_db(query)
+
+# This won't work as this class seems to be built to pull only. I need to think of something else to insert into users.
