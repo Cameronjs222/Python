8ba35733c728c16fcf76d435f047c6159292ee0f
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-03-15 13:31:16 -0700

    build skeleton for user model

--- Flask/Recipe/flask_app/controllers/User_routes.py
+++ Flask/Recipe/flask_app/controllers/User_routes.py
@@ -1,6 +1,6 @@
 from flask_app import app
 from flask import render_template, redirect, request, session
-# from flask_app.models.User_model import User
+from flask_app.models.User_model import User
 # from flask_app.models.Recipe_models import Recipe
 
 @app.route('/')
--- Flask/Recipe/flask_app/models/User_model.py
+++ Flask/Recipe/flask_app/models/User_model.py
@@ -1,2 +1,24 @@
 from flask_app.config.mySQLconnection import connectToMySQL
 
+class User:
+    my_db='recipe'
+    def __init__(self, data):
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data ['password']
+    @classmethod
+    def create_user(cls, data):
+        pass #should check if email is already entered into our system here
+    @classmethod
+    def get_all(cls, data):
+        pass #should use join left in order to merge all the data from the recpies table with the users table in order to display all users and their recipes.
+    @classmethod
+    def update_user_info(cls, data):
+        pass
+    @classmethod
+    def delete_user(cls,data):
+        pass
+    @staticmethod
+    def validation(data):
+        pass
