56d5be90f41258a368438c80d6d7fb0c3b1f3b62
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-03-15 14:44:53 -0700

    create user done, need to create validation model

--- Flask/Recipe/flask_app/controllers/User_routes.py
+++ Flask/Recipe/flask_app/controllers/User_routes.py
@@ -6,5 +6,10 @@ from flask_app.models.User_model import User
 @app.route('/')
 def login_reg():
     return render_template('index.html')
-@app.route('/user/create' methods=['POST'])
+@app.route('/user/create', methods=['POST'])
 def create_new_user():
+    if not User.validation(request.form): #checks if the entered information passes all of the validation specifications.
+        return redirect('/')
+    new_user = User.create_user(request.form) #creates variable for new user that will be saved in session
+    session['new_user'] = new_user
+    return redirect('/post')
