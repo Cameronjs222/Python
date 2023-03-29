f98c2eefb0a9f76909bc837d1de9aad5d2914b53
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-03-28 21:09:08 -0700

    algo

--- test/test.py
+++ test/test.py
@@ -1,1 +1,9 @@
-#setting up files to work on algos and random problems
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        answer = []
+        for i in range(0, len(nums)):
+            for ii in range(i+1, len(nums)):
+                if nums[i] + nums[ii] == target:
+                    answer.append(i)
+                    answer.append(ii)
+                    return answer
