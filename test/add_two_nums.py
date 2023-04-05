1e51565c7b115ba978f606dacc10cf453941f3e9
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-04-04 18:02:43 -0700

    awesome solution for prefix algo!

--- test/add_two_nums.py
+++ test/add_two_nums.py
@@ -1,36 +1,45 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        pre_fixes = {}
-        common_letters = {}
-        greatest = {}
-        # count = 0
-        index = 0
-        for i in range(0, len(strs)):
-            pre_fixes[i] = []
-            common_letters[i] = []
-            greatest[i] = strs[i]
-            print("pre_fixes[i]", pre_fixes[i], i)
-            print("commone_letters[i]", common_letters[i], i)
-            for j in range(0, len(strs[i])):
-                pre_fixes[i].append(strs[i][j])
-                print("pre_fixes[i].append", pre_fixes[i] )
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         pre_fixes = {}
+#         common_letters = {}
+#         greatest = {}
+#         # count = 0
+#         index = 0
+#         for i in range(0, len(strs)):
+#             pre_fixes[i] = []
+#             common_letters[i] = []
+#             greatest[i] = strs[i]
+#             print("pre_fixes[i]", pre_fixes[i], i)
+#             print("commone_letters[i]", common_letters[i], i)
+#             for j in range(0, len(strs[i])):
+#                 pre_fixes[i].append(strs[i][j])
+#                 print("pre_fixes[i].append", pre_fixes[i] )
                 
-                if i > 0:
-                    print(True)
-                    if strs[i-1][j] == pre_fixes[i][j]:
-                        print(True)
-                        common_letters[i].append(strs[i][j])
-                        # count += 1
-                        index = i
-                        greatest[i] = common_letters[i]
-                    else: 
-                        print(True)
-                        greatest[i] = common_letters[i]
-                        # count = 0
-                        index = i
-                        break
-        print(greatest)
-        if len(greatest) > 0:
-            common_prefix = "".join(greatest[index])
-        else: common_prefix = ""
-        return common_prefix
+#                 if i > 0:
+#                     print(True)
+#                     if strs[i-1][j] == pre_fixes[i][j]:
+#                         print(True)
+#                         common_letters[i].append(strs[i][j])
+#                         # count += 1
+#                         index = i
+#                         greatest[i] = common_letters[i]
+#                     else: 
+#                         print(True)
+#                         greatest[i] = common_letters[i]
+#                         # count = 0
+#                         index = i
+#                         break
+#         print(greatest)
+#         if len(greatest) > 0:
+#             common_prefix = "".join(greatest[index])
+#         else: common_prefix = ""
+#         return common_prefix
+class Solustion:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        l = list(zip(*strs))
+        prefix = ""
+        for i in l:
+            if len(set(i)) == 1:
+                prefix += i[0]
+            else: break
+        return prefix
